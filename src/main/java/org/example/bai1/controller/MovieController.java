9836eb3d49cdbd29ce709938a566db9e8b8513b6
chung <youremail@example.com>
2026-05-18 21:05:01 +0700

    data

--- src/main/java/org/example/bai1/controller/MovieController.java
+++ src/main/java/org/example/bai1/controller/MovieController.java
@@ -1,4 +1,26 @@
 package org.example.bai1.controller;
 
+
+import org.example.bai1.model.Movie;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/movies")
 public class MovieController {
+
+    @GetMapping
+    public List<Movie> getMovies() {
+
+        List<Movie> movies = new ArrayList<>();
+
+        movies.add(new Movie("M001", "Inception", "Sci-Fi", 8.8));
+        movies.add(new Movie("M002", "Parasite", "Drama", 8.6));
+
+        return movies;
+    }
 }
--- src/main/java/org/example/bai1/model/Movie.java
+++ src/main/java/org/example/bai1/model/Movie.java
@@ -1,4 +1,52 @@
 package org.example.bai1.model;
 
+
 public class Movie {
+
+    private String movieId;
+    private String title;
+    private String genre;
+    private double rating;
+
+    public Movie() {
+    }
+
+    public Movie(String movieId, String title, String genre, double rating) {
+        this.movieId = movieId;
+        this.title = title;
+        this.genre = genre;
+        this.rating = rating;
+    }
+
+    public String getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(String movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
